e4e4ff1ae9f6b90fc00fc0b4fc153f874a115c22
Shavqat001 <nazriev.shavqatjon@gmail.com>
2024-01-26 15:57:28 +0500

    create webpack

--- source/js/create-fetch.js
+++ source/js/create-fetch.js
@@ -1,5 +1,5 @@
 function fetchData() {
-  return fetch('https://23.javascript.pages.academy/keksobooking/data')
+  return fetch('https://23.javascript.htmlacademy.pro/keksobooking/data')
     .then((response) => {
       if (response.ok) {
         return response.json();
